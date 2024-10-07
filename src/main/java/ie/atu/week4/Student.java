0e7f88c5bfe8683dfad58a4931eeb563ac40bc63
Donal1999 <g00365724@atu.ie>
2024-10-07 10:06:27 +0100

    Created student class and constructors

---
+++ src/main/java/ie/atu/week4/Student.java
@@ -0,0 +1,57 @@
+package ie.atu.week4;
+
+public class Student {
+     private String name ;
+     private String email;
+     private String course;
+
+     public Student() { //default constructor
+          this.name=null;
+          this. email=null;
+          this. course=null;
+     }
+
+     public Student(String name) { // constructor name only parameter
+          this.name = name;
+          this. email=null;
+          this. course=null;
+     }
+
+     public Student(String name, String email, String course) { // constructor that  accepts name,email and course
+          this.name = name;
+          this.email = email;
+          this.course = course;
+     }
+
+     public String getName() {  //getter
+          return name;
+     }
+
+     public void setName(String name) { //setter
+          this.name = name;
+     }
+
+     public String getEmail() {
+          return email;
+     }
+
+     public void setEmail(String email) {
+          this.email = email;
+     }
+
+     public String getCourse() {
+          return course;
+     }
+
+     public void setCourse(String course) {
+          this.course = course;
+     }
+
+     @Override
+     public String toString() { // overriding to get string
+          return "Student details are " +
+                  "name='" + name + '\'' +
+                  ", email='" + email + '\'' +
+                  ", course='" + course + '\'' ;
+     }
+}
